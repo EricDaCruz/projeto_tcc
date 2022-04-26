e69e8a6e6791244571b5e1f9550033d9af7696f9
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-04-26 20:18:06 -0300

    Começando a implementação do bulma

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,13 @@
+
+
+const Error = () => {
+    return(
+        <div>
+            <h1>Error 4040</h1>
+            <p>Página não encontrada</p>
+        </div>
+    )
+
+}
+
+export default Error;
---
+++ src/router.jsx
@@ -0,0 +1,22 @@
+import { 
+    BrowserRouter, 
+    Routes,
+    Route 
+} from "react-router-dom";
+
+/* Pages */
+import Home from './pages/Home'
+import Error from './pages/Error'
+
+const Router = () =>{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
