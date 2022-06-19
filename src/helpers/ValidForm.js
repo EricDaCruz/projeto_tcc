ba303fdaea41eb479020e92099860e4c380c66c6
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-06-19 15:57:10 -0300

    Começando a implementação do validateDateBorn

--- src/helpers/ValidForm.js
+++ src/helpers/ValidForm.js
@@ -1,6 +1,12 @@
 // import validator from "validar-telefone";
 import { parse } from 'telefone'
 
+const validDate = new RegExp("^[0-9]{4}[-][0-9]{2}[-][0-9]{2}$")
+
 export const validatePhoneNumber = (phone) => {
    return parse(phone, { apenasCelular: true });
+}
+export const validateDateBorn = (date) => {
+   return validDate.test(date)
+
 }
