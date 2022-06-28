c436a9687e77d9d9b7504bb7be912ae06eaacfda
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-06-28 10:39:36 -0300

    Switch Case nos erros de login

--- src/components/HomeComponents/LoginArea/index.jsx
+++ src/components/HomeComponents/LoginArea/index.jsx
@@ -37,13 +37,20 @@ export function LoginArea() {
                navigate('/forum')
             }
          }catch(error){
-            console.log(error.code);
-            if(error.code === 'auth/user-not-found'){
-               toast.error('Usuário não encontrado')
-            }else if(error.code === 'auth/wrong-password' || error.code === 'auth/invalid-email'){
-               toast.error('Senha ou usuário inválido')
-            }else if(error.code === 'auth/too-many-requests'){
-               toast.warning('Muitas tentativas inválidas. Tente novamente mais tarde!')
+            switch (error.code){
+               case 'auth/user-not-found':
+                  toast.error('Usuário não encontrado')
+                  break
+               case 'auth/wrong-password':
+               case 'auth/invalid-email':
+                  toast.error('Senha ou usuário inválido')
+                  break
+               case 'auth/too-many-requests':
+                  toast.warning('Muitas tentativas inválidas. Tente novamente mais tarde!')
+                  break
+               case 'auth/network-request-failed':
+                  toast.warning('Verifique a conexão com a internet')
+                  break
             }
          }
       }
