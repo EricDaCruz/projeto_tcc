22c1b6ce6f4c490425f3ffe5dbb10cb9b75cc230
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-07-15 15:25:34 -0300

    Salvando o uid do usuario logado na SessionStorage

--- src/services/LocalStorage.js
+++
@@ -1,9 +0,0 @@
-export const SetItemLocalStorage = (key, value) => {
-    localStorage.setItem(key, value)
-}
-export const GetItemLocalStorage = (key)=> {
-    return localStorage.getItem(key)
-}
-export const RemoveItemLocalStorage = (key) => {
-    localStorage.removeItem(key)
-}
