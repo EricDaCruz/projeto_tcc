1db77c789588e9dc890f462b84a5835687b89e15
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-07-24 13:01:59 -0300

    Implementando o envio das denuncias

--- src/components/Forum/Tooltip/index.jsx
+++ src/components/Forum/Tooltip/index.jsx
@@ -9,21 +9,39 @@ import {
 } from "react-icons/bs";
 import { Container, Button } from "./styles";
 
+import emailjs from "@emailjs/browser";
+
 export const Tooltip = ({ userIdSend, chatUid, commentUid }) => {
    const userId = GetItemSessionStorage("uid");
 
    const deleteQuestion = () => {
-    DeleteQuestions(chatUid).then(toast.success("Pergunta deletada com sucesso!"));
+      DeleteQuestions(chatUid).then(
+         toast.success("Pergunta deletada com sucesso!")
+      );
    };
    const denounceQuestion = () => {
       console.log("Denunciar Pergunta");
    };
 
    const deleteComment = () => {
-      DeleteComments(commentUid).then(toast.success("Comentário deletado com sucesso!"))
+      DeleteComments(commentUid).then(
+         toast.success("Comentário deletado com sucesso!")
+      );
    };
    const denounceComment = () => {
-      console.log("Denunciar Comentário");
+      emailjs.init(import.meta.env.VITE_APP_USER_ID);
+      const template_params = {
+         commentUid: "teste123456",
+         username: "erc",
+      };
+
+      emailjs
+         .send(
+            import.meta.env.VITE_APP_SERVICE_ID,
+            import.meta.env.VITE_APP_COMMENT_TEMPLATE_ID,
+            template_params
+         )
+         .then((result) => console.log(result));
    };
 
    return (
@@ -38,12 +56,19 @@ export const Tooltip = ({ userIdSend, chatUid, commentUid }) => {
       >
          <Container>
             {userIdSend === userId && (
-               <Button action="true" onClick={chatUid ? deleteQuestion : deleteComment}>
+               <Button
+                  action="true"
+                  onClick={chatUid ? deleteQuestion : deleteComment}
+               >
                   <BsTrashFill /> Deletar {chatUid ? "Questão" : "Comentário"}
                </Button>
             )}
-            <Button action="true" onClick={chatUid ? denounceQuestion : denounceComment}>
-               <BsExclamationTriangleFill /> Denunciar {chatUid ? "Questão" : "Comentário"}
+            <Button
+               action="true"
+               onClick={chatUid ? denounceQuestion : denounceComment}
+            >
+               <BsExclamationTriangleFill /> Denunciar{" "}
+               {chatUid ? "Questão" : "Comentário"}
             </Button>
          </Container>
       </Popup>
