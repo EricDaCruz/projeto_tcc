e7d552447f8002c53949f94949770d6bf1d46ab8
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-08-04 20:42:52 -0300

    Continuando layout da tela de profile

--- src/components/Forum/Profile/index.jsx
+++ src/components/Forum/Profile/index.jsx
@@ -1,9 +1,38 @@
+import {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import { Container, ContentInputs, ContentProfile } from './styles';
+import { Field } from '../../SingUp/Field';
+import { SelectDate } from '../../SingUp/SelectDate'; 
 
 export const Profile = (params) => {
     const { userId } = useParams();
+    const [dataBorn, setDataBorn] = useState('');
 
   return(
-    <h1>{userId}</h1>
+    <Container>
+      <ContentProfile>
+        profile
+      </ContentProfile>
+      <ContentInputs>
+        <Field label="Nome">
+          <input type="text" />
+        </Field>
+        <Field label="Email">
+          <input type="email" />
+        </Field>
+        <Field label="Celular">
+          <input type="text" />
+        </Field>
+        <Field label="username">
+          <input type="text" />
+        </Field>
+        <Field label="Data de Nascimento">
+          <SelectDate value={dataBorn} setDateBorn={setDataBorn} />
+        </Field>
+        <Field label="Estado">
+         <input type="text" />
+        </Field>
+      </ContentInputs>
+    </Container>
   )
 }
---
+++ src/components/Forum/Profile/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: red;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+`
+
+export const ContentInputs = styled.div`
+`
+export const ContentProfile = styled.div`
+`
