72a0337528e4bd02a6d62dd75d25e67b12f2301a
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-08-05 19:22:56 -0300

    Colocando a foto de perfil na tela de Profile

--- src/components/Forum/Profile/index.jsx
+++ src/components/Forum/Profile/index.jsx
@@ -1,38 +1,68 @@
-import {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import { Container, ContentInputs, ContentProfile } from './styles';
-import { Field } from '../../SingUp/Field';
-import { SelectDate } from '../../SingUp/SelectDate'; 
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Container, ContentInputs, ContentProfile, Inputs } from "./styles";
+import { Field } from "../../SingUp/Field";
+import { SelectDate } from "../../SingUp/SelectDate";
+import { DropdownStates } from "../../SingUp/Dropdowns/DropdownStates";
+import { DropdownCities } from "../../SingUp/Dropdowns/DropdownCities";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+/* Classes */
+import { User } from "../../../services/User";
 
 export const Profile = (params) => {
-    const { userId } = useParams();
-    const [dataBorn, setDataBorn] = useState('');
+   const { userId } = useParams();
+   const [dateBorn, setDateBorn] = useState("");
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [username, setUsername] = useState("");
+   const [photoUrl, setPhotoUrl] = useState("");
+   const [city, setCity] = useState("");
+   const [state, setState] = useState("");
+   const [phone, setPhone] = useState("");
 
-  return(
-    <Container>
-      <ContentProfile>
-        profile
-      </ContentProfile>
-      <ContentInputs>
-        <Field label="Nome">
-          <input type="text" />
-        </Field>
-        <Field label="Email">
-          <input type="email" />
-        </Field>
-        <Field label="Celular">
-          <input type="text" />
-        </Field>
-        <Field label="username">
-          <input type="text" />
-        </Field>
-        <Field label="Data de Nascimento">
-          <SelectDate value={dataBorn} setDateBorn={setDataBorn} />
-        </Field>
-        <Field label="Estado">
-         <input type="text" />
-        </Field>
-      </ContentInputs>
-    </Container>
-  )
-}
+   useEffect(() => {
+      const user = new User("", userId);
+      user.GetInfoUser().then((data) => {
+         console.log(data);
+         setDateBorn(data.dateBorn);
+         setName(data.name);
+         setEmail(data.email);
+         setUsername(data.username);
+         setPhotoUrl(data.photoUrl);
+         setCity(data.location.city);
+         setState(data.location.state);
+         setPhone(data.phone);
+      });
+   }, []);
+
+   return (
+      <Container>
+         <ContentProfile>
+            <img src={photoUrl} alt="" />
+         </ContentProfile>
+         <ContentInputs>
+            <Field label="Nome">
+               <Inputs type="text" value={name} />
+            </Field>
+            <Field label="Email">
+               <Inputs type="email" value={email} />
+            </Field>
+            <Field label="Celular">
+               <Inputs type="text" value={phone} />
+            </Field>
+            <Field label="username">
+               <Inputs type="text" value={username} />
+            </Field>
+            <Field label="Data de Nascimento">
+               <SelectDate value={dateBorn} setDateBorn={setDateBorn} />
+            </Field>
+            <Field label="Estado">
+               
+            </Field>
+            <Field label="Cidade">
+               
+            </Field>
+         </ContentInputs>
+      </Container>
+   );
+};
--- src/components/Forum/Profile/styles.js
+++ src/components/Forum/Profile/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    padding: 0.5rem 1rem;
     background-color: red;
     display: flex;
     flex-direction: row-reverse;
@@ -8,6 +9,35 @@ export const Container = styled.div`
 `
 
 export const ContentInputs = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    background-color: blue;
+    flex: 1;
 `
 export const ContentProfile = styled.div`
+    min-width: 200px;
+    background-color: green;
+    display: flex;
+    justify-content: center;
+
+    img{
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+`
+
+export const Inputs = styled.input`
+    box-shadow: 0px 2px 6px rgba(19, 18, 66, 0.07);
+    border-radius: 46px;
+    border: 1px solid #EFF0F7;
+    outline: none;
+    padding: 0.75rem 0.5rem ;
+    
+    &:focus{
+       border: 1px solid #1da87a;
+       box-shadow: none;    
+    }
 `
