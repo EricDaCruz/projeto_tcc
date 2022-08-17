9031332b9b273425a6f9a55bf30a27bb6b0961ea
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-08-17 20:33:19 -0300

    Aviso do app em beta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import Router from './router'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export function App() {
-
+  window.alert('O site está em beta, e está disponível somente para testes.');
+  
   return (
     <div className="App">
 
