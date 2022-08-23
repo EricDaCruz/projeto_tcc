76e321e365d7e43d1d3d8a109262d6c6ce3c3575
Eric Rafael <rafaeleric40@gmail.com>
2022-08-23 14:46:36 -0300

    Ordenando as categorias

--- src/assets/categories.js
+++ src/assets/categories.js
@@ -1,261 +1,261 @@
 export const CategoriesSelect = [
    {
-      label: "Sociologia",
-      value: "sociology",
-   },
-   {
-      label: "Química",
-      value: "chemistry",
-   },
-   {
-      label: "Português",
-      value: "portuguese",
-   },
-   {
-      label: "Matemática",
-      value: "math",
-   },
-   {
-      label: "Inglês",
-      value: "english",
-   },
-   {
-      label: "História",
-      value: "history",
-   },
-   {
-      label: "Geografia",
-      value: "geography",
-   },
-   {
-      label: "Física",
-      value: "physics",
-   },
-   {
-      label: "Filosofia",
-      value: "philosophy",
+      label: "Biologia",
+      value: "biology",
    },
    {
       label: "Educação Física",
       value: "physical-education",
    },
    {
-      label: "Biologia",
-      value: "biology",
+      label: "Filosofia",
+      value: "philosophy",
    },
    {
-      label: "TDI - RTM",
-      value: "tdi-rtm",
+      label: "Física",
+      value: "physics",
    },
    {
-      label: "TDI - RLPA",
-      value: "tdi-rlpa",
+      label: "Geografia",
+      value: "geography",
    },
    {
-      label: "TDI - RDPI",
-      value: "tdi-rdpi",
+      label: "História",
+      value: "history",
    },
    {
-      label: "TDI - PIR",
-      value: "tdi-pir",
+      label: "Inglês",
+      value: "english",
    },
    {
-      label: "TDI - PIC",
-      value: "tdi-pic",
+      label: "Matemática",
+      value: "math",
    },
    {
-      label: "TDI - PEE",
-      value: "tdi-pee",
+      label: "Português",
+      value: "portuguese",
    },
    {
-      label: "TDI - IPI",
-      value: "tdi-ipi",
+      label: "Química",
+      value: "chemistry",
    },
    {
-      label: "TDI - História do Mob",
-      value: "tdi-history-do-mob",
+      label: "Sociologia",
+      value: "sociology",
    },
    {
-      label: "TDI - EVADI",
-      value: "tdi-evadi",
-   },
-   {
-      label: "TDI - Ergonomia",
-      value: "tdi-ergonomia",
-   },
-   {
-      label: "TDI - CPDI",
-      value: "tdi-cpdi",
-   },
-   {
-      label: "TDI - CAAPI",
-      value: "tdi-caapi",
-   },
-   {
-      label: "TDI - EAMRDI",
-      value: "tdi-eamrdi",
-   },
-   {
-      label: "TMA - UOS",
-      value: "tma-uos",
-   },
-   {
-      label: "TMA - STAR",
-      value: "tma-star",
-   },
-   {
-      label: "TMA - PQA",
-      value: "tma-pqa",
-   },
-   {
-      label: "TMA - PEA",
-      value: "tma-pea",
-   },
-   {
-      label: "TMA - TP",
-      value: "tma-tp",
-   },
-   {
-      label: "TMA - PCT",
-      value: "tma-pct",
-   },
-   {
-      label: "TMA - PASP",
-      value: "tma-pasp",
-   },
-   {
-      label: "TMA - PAMC",
-      value: "tma-pamc",
-   },
-   {
-      label: "TMA - MRV",
-      value: "tma-mrv",
-   },
-   {
-      label: "TMA - LEI",
-      value: "tma-lei",
-   },
-   {
-      label: "TMA - GQIA",
-      value: "tma-gqia",
-   },
-   {
-      label: "TMA - EMA",
-      value: "tma-ema",
-   },
-   {
-      label: "TMA - DS",
-      value: "tma-ds",
-   },
-   {
-      label: "TMA - AMAAS",
-      value: "tma-amaas",
-   },
-   {
-      label: "TMA - AFQAE",
-      value: "tma-afqae",
-   },
-   {
-      label: "DS - TPA",
-      value: "ds-tpa",
-   },
-   {
-      label: "DS - SE",
-      value: "ds-se",
-   },
-   {
-      label: "DS - QTS",
-      value: "ds-qts",
-   },
-   {
-      label: "DS - PW",
-      value: "ds-pw",
-   },
-   {
-      label: "DS - PAM",
-      value: "ds-pam",
-   },
-   {
-      label: "DS - IPSSI",
-      value: "ds-ipssi",
-   },
-   {
-      label: "DS - FI",
-      value: "ds-fi",
-   },
-   {
-      label: "DS - DS",
-      value: "ds-ds",
-   },
-   {
-      label: "DS - DD",
-      value: "ds-dd",
+      label: "DS - APS",
+      value: "ds-aps",
    },
    {
       label: "DS - BD",
       value: "ds-bd",
    },
    {
-      label: "DS - APS",
-      value: "ds-aps",
+      label: "DS - DD",
+      value: "ds-dd",
    },
-];
+   {
+      label: "DS - DS",
+      value: "ds-ds",
+   },
+   {
+      label: "DS - FI",
+      value: "ds-fi",
+   },
+   {
+      label: "DS - IPSSI",
+      value: "ds-ipssi",
+   },
+   {
+      label: "DS - PAM",
+      value: "ds-pam",
+   },
+   {
+      label: "DS - PW",
+      value: "ds-pw",
+   },
+   {
+      label: "DS - QTS",
+      value: "ds-qts",
+   },
+   {
+      label: "DS - SE",
+      value: "ds-se",
+   },
+   {
+      label: "DS - TPA",
+      value: "ds-tpa",
+   },
+   {
+      label: "TDI - CAAPI",
+      value: "tdi-caapi",
+   },
+   {
+      label: "TDI - CPDI",
+      value: "tdi-cpdi",
+   },
+   {
+      label: "TDI - EAMRDI",
+      value: "tdi-eamrdi",
+   },
+   {
+      label: "TDI - Ergonomia",
+      value: "tdi-ergonomia",
+   },
+   {
+      label: "TDI - EVADI",
+      value: "tdi-evadi",
+   },
+   {
+      label: "TDI - História do Mob",
+      value: "tdi-history-do-mob",
+   },
+   {
+      label: "TDI - IPI",
+      value: "tdi-ipi",
+   },
+   {
+      label: "TDI - PEE",
+      value: "tdi-pee",
+   },
+   {
+      label: "TDI - PIC",
+      value: "tdi-pic",
+   },
+   {
+      label: "TDI - PIR",
+      value: "tdi-pir",
+   },
+   {
+      label: "TDI - RDPI",
+      value: "tdi-rdpi",
+   },
+   {
+      label: "TDI - RLPA",
+      value: "tdi-rlpa",
+   },
+   {
+      label: "TDI - RTM",
+      value: "tdi-rtm",
+   },
+   {
+      label: "TMA - AFQAE",
+      value: "tma-afqae",
+   },
+   {
+      label: "TMA - AMAAS",
+      value: "tma-amaas",
+   },
+   {
+      label: "TMA - DS",
+      value: "tma-ds",
+   }, 
+   {
+      label: "TMA - EMA",
+      value: "tma-ema",
+   },
+   {
+      label: "TMA - GQIA",
+      value: "tma-gqia",
+   },
+   {
+      label: "TMA - LEI",
+      value: "tma-lei",
+   },
+   {
+      label: "TMA - MRV",
+      value: "tma-mrv",
+   },
+   {
+      label: "TMA - PAMC",
+      value: "tma-pamc",
+   },
+   {
+      label: "TMA - PASP",
+      value: "tma-pasp",
+   },
+   {
+      label: "TMA - PCT",
+      value: "tma-pct",
+   },
+   {
+      label: "TMA - PEA",
+      value: "tma-pea",
+   },
+   {
+      label: "TMA - PQA",
+      value: "tma-pqa",
+   },
+   {
+      label: "TMA - STAR",
+      value: "tma-star",
+   },
+   {
+      label: "TMA - TP",
+      value: "tma-tp",
+   },
+   {
+      label: "TMA - UOS",
+      value: "tma-uos",
+   },
 
+];
 export const Categories = [
    {
-      label: "Sociologia",
-      value: "sociology",
-      src: "soc.png",
-   },
-   {
-      label: "Química",
-      value: "chemistry",
-      src: "qui.png",
-   },
-   {
-      label: "Português",
-      value: "portuguese",
-      src: "port.png",
-   },
-   {
-      label: "Matemática",
-      value: "math",
-      src: "mat.png",
-   },
-   {
-      label: "Inglês",
-      value: "english",
-      src: "ing.png",
-   },
-   {
-      label: "História",
-      value: "history",
-      src: "his.png",
-   },
-   {
-      label: "Geografia",
-      value: "geography",
-      src: "geo.png",
-   },
-   {
-      label: "Física",
-      value: "physics",
-      src: "fis.png",
-   },
-   {
-      label: "Filosofia",
-      value: "philosophy",
-      src: "fil.png",
+      label: "Biologia",
+      value: "biology",
+      src: "bio.png"
    },
    {
       label: "Educação Física",
       value: "physical-education",
-      src: "edf.png",
+      src: "edf.png"
    },
    {
-      label: "Biologia",
-      value: "biology",
-      src: "bio.png",
+      label: "Filosofia",
+      value: "philosophy",
+      src: "fil.png"
+   },
+   {
+      label: "Física",
+      value: "physics",
+      src: "fis.png"
+   },
+   {
+      label: "Geografia",
+      value: "geography",
+      src: "geo.png"
+   },
+   {
+      label: "História",
+      value: "history",
+      src: "his.png"
+   },
+   {
+      label: "Inglês",
+      value: "english",
+      src: "ing.png"
+   },
+   {
+      label: "Matemática",
+      value: "math",
+      src: "mat.png"
+   },
+   {
+      label: "Português",
+      value: "portuguese",
+      src: "port.png"
+   },
+   {
+      label: "Química",
+      value: "chemistry",
+      src: "qui.png"
+   },
+   {
+      label: "Sociologia",
+      value: "sociology",
+      src: "soc.png"
    },
    {
       label: "Desenvolvimento de Sistemas",
@@ -263,217 +263,211 @@ export const Categories = [
       src: "ds.png",
    },
    {
-      label: "Técnico em Meio Ambiente",
-      value: "tma",
-      src: "tma.png",
-   },
-   {
       label: "Técnico em Design de Interiores",
       value: "tdi",
       src: "tdi.png",
    },
+   {
+      label: "Técnico em Meio Ambiente",
+      value: "tma",
+      src: "tma.png",
+   },
 ];
-
 export const CategoriesBySubject = {
    ds: [
       {
-         label: "DS - TPA",
-         value: "ds-tpa",
-         src: "ds-tpa.png",
-      },
-      {
-         label: "DS - SE",
-         value: "ds-se",
-         src: "ds-se.png",
-      },
-      {
-         label: "DS - QTS",
-         value: "ds-qts",
-         src: "ds-qts.png",
-      },
-      {
-         label: "DS - PW",
-         value: "ds-pw",
-         src: "ds-pw.png",
-      },
-      {
-         label: "DS - PAM",
-         value: "ds-pam",
-         src: "ds-pam.png",
-      },
-      {
-         label: "DS - IPSSI",
-         value: "ds-ipssi",
-         src: "ds-ipssi.png",
-      },
-      {
-         label: "DS - FI",
-         value: "ds-fi",
-         src: "ds-fi.png",
-      },
-      {
-         label: "DS - DS",
-         value: "ds-ds",
-         src: "ds-ds.png",
-      },
-      {
-         label: "DS - DD",
-         value: "ds-dd",
-         src: "ds-dd.png",
+         label: "DS - APS",
+         value: "ds-aps",
+         src: "ds-aps.png"
       },
       {
          label: "DS - BD",
          value: "ds-bd",
-         src: "ds-bd.png",
+         src: "ds-bd.png"
       },
       {
-         label: "DS - APS",
-         value: "ds-aps",
-         src: "ds-aps.png",
+         label: "DS - DD",
+         value: "ds-dd",
+         src: "ds-dd.png"
+      },
+      {
+         label: "DS - DS",
+         value: "ds-ds",
+         src: "ds-ds.png"
+      },
+      {
+         label: "DS - FI",
+         value: "ds-fi",
+         src: "ds-fi.png"
+      },
+      {
+         label: "DS - IPSSI",
+         value: "ds-ipssi",
+         src: "ds-ipssi.png"
+      },
+      {
+         label: "DS - PAM",
+         value: "ds-pam",
+         src: "ds-pam.png"
+      },
+      {
+         label: "DS - PW",
+         value: "ds-pw",
+         src: "ds-pw.png"
+      },
+      {
+         label: "DS - QTS",
+         value: "ds-qts",
+         src: "ds-qts.png"
+      },
+      {
+         label: "DS - SE",
+         value: "ds-se",
+         src: "ds-se.png"
+      },
+      {
+         label: "DS - TPA",
+         value: "ds-tpa",
+         src: "ds-tpa.png"
       },
    ],
    tdi: [
       {
-         label: "TDI - RTM",
-         value: "tdi-rtm",
-         src: "tdi-rtm.png",
-      },
-      {
-         label: "TDI - RLPA",
-         value: "tdi-rlpa",
-         src: "tdi-rlpa.png",
-      },
-      {
-         label: "TDI - RDPI",
-         value: "tdi-rdpi",
-         src: "tdi-rdpi.png",
-      },
-      {
-         label: "TDI - PIR",
-         value: "tdi-pir",
-         src: "tdi-pir.png",
-      },
-      {
-         label: "TDI - PIC",
-         value: "tdi-pic",
-         src: "tdi-pic.png",
-      },
-      {
-         label: "TDI - PEE",
-         value: "tdi-pee",
-         src: "tdi-pee.png",
-      },
-      {
-         label: "TDI - IPI",
-         value: "tdi-ipi",
-         src: "tdi-ipi.png",
-      },
-      {
-         label: "TDI - História do Mob",
-         value: "tdi-historia-do-mob",
-         src: "tdi-historia_do_mob.png",
-      },
-      {
-         label: "TDI - EVADI",
-         value: "tdi-evadi",
-         src: "tdi-evadi.png",
-      },
-      {
-         label: "TDI - Ergonomia",
-         value: "tdi-ergonomia",
-         src: "tdi-ergonomia.png",
+         label: "TDI - CAAPI",
+         value: "tdi-caapi",
+         src: "tdi-caapi.png"
       },
       {
          label: "TDI - CPDI",
          value: "tdi-cpdi",
-         src: "tdi-cpdi.png",
-      },
-      {
-         label: "TDI - CAAPI",
-         value: "tdi-caapi",
-         src: "tdi-caapi.png",
+         src: "tdi-cpdi.png"
       },
       {
          label: "TDI - EAMRDI",
          value: "tdi-eamrdi",
-         src: "tdi-eamrdi.png",
+         src: "tdi-eamrdi.png"
+      },
+      {
+         label: "TDI - Ergonomia",
+         value: "tdi-ergonomia",
+         src: "tdi-ergonomia.png"
+      },
+      {
+         label: "TDI - EVADI",
+         value: "tdi-evadi",
+         src: "tdi-evadi.png"
+      },
+      {
+         label: "TDI - História do Mob",
+         value: "tdi-history-do-mob",
+         src: "tdi-historia_do_mob.png"
+      },
+      {
+         label: "TDI - IPI",
+         value: "tdi-ipi",
+         src: "tdi-ipi.png"
+      },
+      {
+         label: "TDI - PEE",
+         value: "tdi-pee",
+         src: "tdi-pee.png"
+      },
+      {
+         label: "TDI - PIC",
+         value: "tdi-pic",
+         src: "tdi-pic.png"
+      },
+      {
+         label: "TDI - PIR",
+         value: "tdi-pir",
+         src: "tdi-pir.png"
+      },
+      {
+         label: "TDI - RDPI",
+         value: "tdi-rdpi",
+         src: "tdi-rdpi.png"
+      },
+      {
+         label: "TDI - RLPA",
+         value: "tdi-rlpa",
+         src: "tdi-rlpa.png"
+      },
+      {
+         label: "TDI - RTM",
+         value: "tdi-rtm",
+         src: "tdi-rtm.png"
       },
    ],
    tma: [
       {
-         label: "TMA - UOS",
-         value: "tma-uos",
-         src: "tma-uos.png",
-      },
-      {
-         label: "TMA - STAR",
-         value: "tma-star",
-         src: "tma-star.png",
-      },
-      {
-         label: "TMA - PQA",
-         value: "tma-pqa",
-         src: "tma-pqa.png",
-      },
-      {
-         label: "TMA - PEA",
-         value: "tma-pea",
-         src: "tma-pea.png",
-      },
-      {
-         label: "TMA - TP",
-         value: "tma-tp",
-         src: "tma-tp.png",
-      },
-      {
-         label: "TMA - PCT",
-         value: "tma-pct",
-         src: "tma-pct.png",
-      },
-      {
-         label: "TMA - PASP",
-         value: "tma-pasp",
-         src: "tma-pasp.png",
-      },
-      {
-         label: "TMA - PAMC",
-         value: "tma-pamc",
-         src: "tma-pamc.png",
-      },
-      {
-         label: "TMA - MRV",
-         value: "tma-mrv",
-         src: "tma-mrv.png",
-      },
-      {
-         label: "TMA - LEI",
-         value: "tma-lei",
-         src: "tma-lei.png",
-      },
-      {
-         label: "TMA - GQIA",
-         value: "tma-gqia",
-         src: "tma-gqia.png",
-      },
-      {
-         label: "TMA - EMA",
-         value: "tma-ema",
-         src: "tma-ema.png",
+         label: "TMA - AMAAS",
+         value: "tma-amaas",
+         src: "tma-amaas.png"
       },
       {
          label: "TMA - DS",
          value: "tma-ds",
-         src: "tma-ds.png",
+         src: "tma-ds.png"
+      }, 
+      {
+         label: "TMA - EMA",
+         value: "tma-ema",
+         src: "tma-ema.png"
       },
       {
-         label: "TMA - AMAAS",
-         value: "tma-amaas",
-         src: "tma-amaas.png",
+         label: "TMA - GQIA",
+         value: "tma-gqia",
+         src: "tma-gqia.png"
       },
       {
-         label: "TMA - AFQAE",
-         value: "tma-afqae",
-         src: "tma-afqae.png",
+         label: "TMA - LEI",
+         value: "tma-lei",
+         src: "tma-lei.png"
+      },
+      {
+         label: "TMA - MRV",
+         value: "tma-mrv",
+         src: "tma-mrv.png"
+      },
+      {
+         label: "TMA - PAMC",
+         value: "tma-pamc",
+         src: "tma-pamc.png"
+      },
+      {
+         label: "TMA - PASP",
+         value: "tma-pasp",
+         src: "tma-pasp.png"
+      },
+      {
+         label: "TMA - PCT",
+         value: "tma-pct",
+         src: "tma-pct.png"
+      },
+      {
+         label: "TMA - PEA",
+         value: "tma-pea",
+         src: "tma-pea.png"
+      },
+      {
+         label: "TMA - PQA",
+         value: "tma-pqa",
+         src: "tma-pqa.png"
+      },
+      {
+         label: "TMA - STAR",
+         value: "tma-star",
+         src: "tma-star.png"
+      },
+      {
+         label: "TMA - TP",
+         value: "tma-tp",
+         src: "tma-tp.png"
+      },
+      {
+         label: "TMA - UOS",
+         value: "tma-uos",
+         src: "tma-uos.png"
       },
    ],
 };
