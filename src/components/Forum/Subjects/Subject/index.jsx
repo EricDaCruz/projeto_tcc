521321d1515d90b6ed60905ad2960d7e8fcbae6c
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-09-06 09:30:28 -0300

    Mostrando imagens no subjects

--- src/components/Forum/Subjects/Subject/index.jsx
+++ src/components/Forum/Subjects/Subject/index.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Questions } from "../../Questions";
 import { LoaderQuestion } from "../../../LoaderQuestion";
+import { useQuestions } from "../../../../contexts/QuestionsContext"; 
 /* Classes */
 import { Question } from "../../../../services/Question";
 
 export const Subject = () => {
    const { category } = useParams();
-
+   const {questions, setQuestions} = useQuestions()
    const [subjects, setSubjects] = useState([]);
    const [loading, setLoading] = useState(true);
 
@@ -15,31 +16,27 @@ export const Subject = () => {
       setLoading(true);
       const question = new Question();
       question.GetQuestionsByCategory(category).then((data) => {
-         setSubjects(data);
+         setQuestions(data);
          setLoading(false);
       })
    }, []);
-
-   if (subjects.length !== 0) {
-      console.log(subjects);
-   }
 
    return (
       <>
          {loading ? (
             <LoaderQuestion />
-         ) : subjects.length !== 0 ? (
-            subjects.map((question) => {
-               // const formatContent = question.content.split('\n\n')
+         ) : questions.length !== 0 ? (
+            questions.map((quest) => {
                return (
                   <Questions
-                     key={question.questionUid}
-                     title={question.title}
-                     content={question.content}
-                     postDate={question.postDate}
-                     stars={question.stars}
-                     userId={question.userId}
-                     questionUid={question.questionUid}
+                     key={quest.questionUid}
+                     title={quest.title}
+                     content={quest.content}
+                     postDate={quest.postDate}
+                     stars={quest.stars}
+                     userId={quest.userId}
+                     questionUid={quest.questionUid}
+                     image={quest.image}
                   />
                );
             })
