b8c71d3f55cdf0fda51aa3c8333934c24e1b1e35
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-09-26 19:13:10 -0300

    Arrumando o font-size para dispositivos móveis

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export function App() {
-  window.alert('O site está em beta, e está disponível somente para testes.');
-  
   return (
     <div className="App">
 
