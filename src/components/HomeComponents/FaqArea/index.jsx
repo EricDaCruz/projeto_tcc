399ac6d0831f734c7691ebb7900778639536c5c6
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-10-30 11:03:40 -0300

    Slider FaqArea

--- src/components/HomeComponents/FaqArea/index.jsx
+++ src/components/HomeComponents/FaqArea/index.jsx
@@ -1,19 +1,111 @@
-import { ContetnImg } from "./styles"
-import Duvidas from '../../../assets/images/other-bg-questions.png'
-import DuvidasEt from '../../../assets/images/duvidas-frequentes.png'
+import { ContetnImg, SlideFaq } from "./styles";
+import Duvidas from "../../../assets/images/other-bg-questions.png";
+import DuvidasEt from "../../../assets/images/duvidas-frequentes.png";
+/* Slider */
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
-export function FaqArea (){
-    return(
-        <div className="container columns is-fluid is-flex is-flex-direction-column is-hidden-mobile is-hidden-tablet-only" style={{backgroundColor: '#E8F4F0', margin: '0'}}>
-            <h2 className="column is-full has-text-centered has-text-weight-bold is-size-3" style={{color: '#2B3742'}}>Dúvidas Frequentes</h2>
-            <div className="is-flex " style={{height: '100%'}}>
-                <ContetnImg className="contentImage column is-half">
-                <img src={Duvidas} className="questions"/>
-                </ContetnImg>
-                <ContetnImg className="contentImage column">
-                    <img src={DuvidasEt} />
-                </ContetnImg>
+const carousel = (slider) => {
+   const z = 300;
+   function rotate() {
+      const deg = 360 * slider.track.details.progress;
+      slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
+   }
+   slider.on("created", () => {
+      const deg = 360 / slider.slides.length;
+      slider.slides.forEach((element, idx) => {
+         element.style.transform = `rotateY(${
+            deg * idx
+         }deg) translateZ(${z}px)`;
+      });
+      rotate();
+   });
+   slider.on("detailsChanged", rotate);
+};
+
+export function FaqArea() {
+   const [sliderRef, instanceRef] = useKeenSlider(
+      {
+         loop: true,
+         selector: ".carousel__cell",
+         renderMode: "custom",
+         mode: "free-snap",
+      },
+      [carousel]
+   );
+
+   return (
+      <div
+         className="container columns is-fluid is-flex is-flex-direction-column is-hidden-mobile is-hidden-tablet-only"
+         style={{ backgroundColor: "#E8F4F0", margin: "0" }}
+      >
+         <h2
+            className="column is-full has-text-centered has-text-weight-bold is-size-3"
+            style={{ color: "#2B3742" }}
+         >
+            Dúvidas Frequentes
+         </h2>
+         <div
+            className="columns has-background-primary	"
+            style={{ height: "100%" }}
+         >
+            <SlideFaq className="column is-three-fifths ">
+               <div className="wrapper">
+                  <div className="scene">
+                     <div className="carousel keen-slider" ref={sliderRef}>
+                        <div className="carousel__cell number-slide">
+                           <h3>As matérias estão completas?</h3>
+                           <p>
+                              Os materiais didáticos estão sempre sendo
+                              atualizados e estão todos completos, com todas as
+                              informações para a qualidade do seu estudo.
+                           </p>
+                        </div>
+                        <div className="carousel__cell number-slide">
+                           <h3>Qual a importância de um fórum de estudo?</h3>
+                           <p>
+                              Por meio dessa ferramenta, os alunos têm a
+                              oportunidade de compartilhar as suas dúvidas,
+                              ajudar a solucionar os questionamentos dos colegas
+                              e a construir, coletivamente, vários tipos de
+                              conhecimento.
+                           </p>
+                        </div>
+                        <div className="carousel__cell number-slide">
+                           <h3>
+                              Dentro do fórum, eu consigo responder
+                              questões relacionadas a qualquer matéria?
+                           </h3>
+                           <p>
+                              Sim, com certeza! Dentro das ferramentas você pode
+                              responder qualquer pergunta que já foi feita e assim compartilhar
+                              informações e conteúdo.
+                           </p>
+                        </div>
+                        <div className="carousel__cell number-slide">
+                           <h3>Posso perguntar sobre qualquer matéria?</h3>
+                           <p>
+                              Sim, com certeza! Dentro das ferramentas você pode
+                              fazer uma pergunta e responder outras, de diversas
+                              matérias.
+                           </p>
+                        </div>
+                        <div className="carousel__cell number-slide">
+                           <h3>É difícil de usar?</h3>
+                           <p>
+                              Nem um pouco! A interação do site foi pensada para
+                              ser prática e visual, independentemente do nível
+                              de conhecimento do usuário.
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </SlideFaq>
+            <div className="contentImage column is-two-fifths">
+               <img src={DuvidasEt} />
             </div>
-        </div>
-    )
+         </div>
+      </div>
+   );
 }
--- src/components/HomeComponents/FaqArea/styles.js
+++ src/components/HomeComponents/FaqArea/styles.js
@@ -1,15 +1,74 @@
 import styled from "styled-components";
 
 export const ContetnImg = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+   display: flex;
+   align-items: center;
+   justify-content: center;
 
-    img{
-        width: 85%;
-    }
-    img.questions{
-        width: 70%
-    }
+   img {
+      width: 85%;
+   }
+   img.questions {
+      width: 70%;
+   }
+`;
 
-`
+export const SlideFaq = styled.div`
+   padding: 0;
+
+   [class^="number-slide"],
+   [class*=" number-slide"] {
+      background: #fff;
+      font-size: 50px;
+      color: #000;
+      font-weight: 500;
+      height: 200px;
+      max-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      border-radius: 8px;
+      padding: 0.75rem;
+    
+
+      h3 {
+         font-size: 1.25rem;
+         margin-bottom: 1rem;
+         font-weight: 700;
+      }
+      p {
+         font-size: 1rem;
+         font-weight: 400;
+      }
+   }
+
+   .wrapper {
+      display: flex;
+      justify-content: center;
+      /* align-items: center; */
+      padding-top: 15%;
+      height: 100%;
+   }
+   .scene {
+      width: 260px;
+      height: 200px;
+      perspective: 1000px;
+      position: relative;
+   }
+   .carousel {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      overflow: visible;
+      transform: translateZ(-288px);
+      transform-style: preserve-3d;
+   }
+   .carousel__cell {
+      position: absolute;
+      width: 350px;
+      left: 10px;
+      height: 300px;
+      border: 1px solid rgba(0, 0, 0, 0.3);
+   }
+`;
