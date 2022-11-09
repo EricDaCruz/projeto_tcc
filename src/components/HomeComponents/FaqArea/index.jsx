9bc98ee2dcb9d8a3f89e3ecad3b712e0ed940816
Eric <rafaeleric40@gmail.com>
2022-11-09 14:05:11 -0300

    Slide da área de Faq

--- src/components/HomeComponents/FaqArea/index.jsx
+++ src/components/HomeComponents/FaqArea/index.jsx
@@ -1,111 +1,158 @@
-import { ContetnImg, SlideFaq } from "./styles";
-import Duvidas from "../../../assets/images/other-bg-questions.png";
+import { useState } from "react";
+import { SlideFaq } from "./styles";
 import DuvidasEt from "../../../assets/images/duvidas-frequentes.png";
 /* Slider */
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
-const carousel = (slider) => {
-   const z = 300;
-   function rotate() {
-      const deg = 360 * slider.track.details.progress;
-      slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
-   }
-   slider.on("created", () => {
-      const deg = 360 / slider.slides.length;
-      slider.slides.forEach((element, idx) => {
-         element.style.transform = `rotateY(${
-            deg * idx
-         }deg) translateZ(${z}px)`;
-      });
-      rotate();
-   });
-   slider.on("detailsChanged", rotate);
-};
+function Arrow(props) {
+  const disabeld = props.disabled ? " arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabeld}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  );
+}
 
 export function FaqArea() {
-   const [sliderRef, instanceRef] = useKeenSlider(
-      {
-         loop: true,
-         selector: ".carousel__cell",
-         renderMode: "custom",
-         mode: "free-snap",
-      },
-      [carousel]
-   );
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
 
-   return (
-      <div
-         className="container columns is-fluid is-flex is-flex-direction-column is-hidden-mobile is-hidden-tablet-only"
-         style={{ backgroundColor: "#E8F4F0", margin: "0" }}
+  return (
+    <div
+      className="container columns is-fluid is-flex is-flex-direction-column is-hidden-mobile is-hidden-tablet-only"
+      style={{ backgroundColor: "#E8F4F0", margin: "0" }}
+    >
+      <h2
+        className="column is-full has-text-centered has-text-weight-bold is-size-3"
+        style={{ color: "#2B3742" }}
       >
-         <h2
-            className="column is-full has-text-centered has-text-weight-bold is-size-3"
-            style={{ color: "#2B3742" }}
-         >
-            Dúvidas Frequentes
-         </h2>
-         <div
-            className="columns"
-            style={{ height: "100%" }}
-         >
-            <SlideFaq className="column is-three-fifths ">
-               <div className="wrapper">
-                  <div className="scene">
-                     <div className="carousel keen-slider" ref={sliderRef}>
-                        <div className="carousel__cell number-slide">
-                           <h3>As matérias estão completas?</h3>
-                           <p>
-                              Os materiais didáticos estão sempre sendo
-                              atualizados e estão todos completos, com todas as
-                              informações para a qualidade do seu estudo.
-                           </p>
-                        </div>
-                        <div className="carousel__cell number-slide">
-                           <h3>Qual a importância de um fórum de estudo?</h3>
-                           <p>
-                              Por meio dessa ferramenta, os alunos têm a
-                              oportunidade de compartilhar as suas dúvidas,
-                              ajudar a solucionar os questionamentos dos colegas
-                              e a construir, coletivamente, vários tipos de
-                              conhecimento.
-                           </p>
-                        </div>
-                        <div className="carousel__cell number-slide">
-                           <h3>
-                              Dentro do fórum, eu consigo responder
-                              questões relacionadas a qualquer matéria?
-                           </h3>
-                           <p>
-                              Sim, com certeza! Dentro das ferramentas você pode
-                              responder qualquer pergunta que já foi feita e assim compartilhar
-                              informações e conteúdo.
-                           </p>
-                        </div>
-                        <div className="carousel__cell number-slide">
-                           <h3>Posso perguntar sobre qualquer matéria?</h3>
-                           <p>
-                              Sim, com certeza! Dentro das ferramentas você pode
-                              fazer uma pergunta e responder outras, de diversas
-                              matérias.
-                           </p>
-                        </div>
-                        <div className="carousel__cell number-slide">
-                           <h3>É difícil de usar?</h3>
-                           <p>
-                              Nem um pouco! A interação do site foi pensada para
-                              ser prática e visual, independentemente do nível
-                              de conhecimento do usuário.
-                           </p>
-                        </div>
-                     </div>
+        Dúvidas Frequentes
+      </h2>
+      <div className="columns" style={{ height: "100%" }}>
+        <SlideFaq className="column is-three-fifths ">
+          <div className="container-slider">
+            <div className="navigation-wrapper">
+              <div ref={sliderRef} className="keen-slider">
+                <div className="keen-slider__slide number-slide1">
+                  <div className="content-text">
+                     <h3>
+                     As matérias estão completas?
+                     </h3>
+                     <p>Os materiais didáticos estão sempre sendo atualizados e estão todos completos, com todas as informações para a qualidade do seu estudo.</p>
                   </div>
-               </div>
-            </SlideFaq>
-            <div className="contentImage column is-two-fifths">
-               <img src={DuvidasEt} />
+                </div>
+                <div className="keen-slider__slide number-slide2">
+                  <div className="content-text">
+                     <h3>
+                     Qual a importância de um fórum de estudo? 
+                     </h3>
+                     <p>
+                     Por meio dessa ferramenta, os alunos têm a oportunidade de compartilhar as suas dúvidas, ajudar a solucionar os questionamentos dos colegas e a construir, coletivamente, vários tipos de conhecimento.
+                     </p>
+                  </div>
+                </div>
+                <div className="keen-slider__slide number-slide3">
+                  <div className="content-text">
+                     <h3>
+                     Dentro do fórum, eu consigo responder questões relacionadas a qualquer matéria?
+                     </h3>
+                     <p>
+                     Sim, com certeza! Dentro das ferramentas você pode responder qualquer pergunta que já foi feita e assim compartilhar informações e conteúdo.
+                     </p>
+                  </div>
+                </div>
+                <div className="keen-slider__slide number-slide4">
+                  <div className="content-text">
+                     <h3>
+                     Posso perguntar sobre qualquer matéria?
+                     </h3>
+                     <p>
+                     Sim, com certeza! Dentro das ferramentas você pode fazer uma pergunta e responder outras, de diversas matérias.
+                     </p>
+                  </div>
+                </div>
+                <div className="keen-slider__slide number-slide5">
+                  <div className="content-text">
+                     <h3>
+                     É difícil de usar?
+                     </h3>
+                     <p>
+                     Nem um pouco! A interação do site foi pensada para ser prática e visual, independentemente do nível de conhecimento do usuário.
+                     </p>
+                  </div>
+                </div>
+              </div>
+              {loaded && instanceRef.current && (
+                <>
+                  <Arrow
+                    left
+                    onClick={(e) =>
+                      e.stopPropagation() || instanceRef.current?.prev()
+                    }
+                    disabled={currentSlide === 0}
+                  />
+
+                  <Arrow
+                    onClick={(e) =>
+                      e.stopPropagation() || instanceRef.current?.next()
+                    }
+                    disabled={
+                      currentSlide ===
+                      instanceRef.current.track.details.slides.length - 1
+                    }
+                  />
+                </>
+              )}
             </div>
-         </div>
+            {loaded && instanceRef.current && (
+              <div className="dots">
+                {[
+                  ...Array(
+                    instanceRef.current.track.details.slides.length
+                  ).keys(),
+                ].map((idx) => {
+                  return (
+                    <button
+                      key={idx}
+                      onClick={() => {
+                        instanceRef.current?.moveToIdx(idx);
+                      }}
+                      className={
+                        "dot" + (currentSlide === idx ? " active" : "")
+                      }
+                    ></button>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </SlideFaq>
+        <div className="contentImage column is-two-fifths">
+          <img src={DuvidasEt} />
+        </div>
       </div>
-   );
+    </div>
+  );
 }
--- src/components/HomeComponents/FaqArea/styles.js
+++ src/components/HomeComponents/FaqArea/styles.js
@@ -1,74 +1,108 @@
 import styled from "styled-components";
 
 export const ContetnImg = styled.div`
-   display: flex;
-   align-items: center;
-   justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-   img {
-      width: 85%;
-   }
-   img.questions {
-      width: 70%;
-   }
+  img {
+    width: 85%;
+  }
+  img.questions {
+    width: 70%;
+  }
 `;
 
 export const SlideFaq = styled.div`
-   padding: 0;
+  .container-slider {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 
-   [class^="number-slide"],
-   [class*=" number-slide"] {
-      background: #fff;
-      font-size: 50px;
+  .keen-slider {
+    height: 400px;
+    border-radius: 10px;
+  }
+
+  [class^="number-slide"],
+  [class*=" number-slide"] {
+    background: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 50px;
+    color: #fff;
+    font-weight: 500;
+    height: 300px;
+    max-height: 100vh;
+
+    .content-text{
       color: #000;
-      font-weight: 500;
-      height: 200px;
-      max-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
       text-align: center;
-      border-radius: 8px;
-      padding: 0.75rem;
-    
+      max-width: 80%;
 
-      h3 {
-         font-size: 1.25rem;
-         margin-bottom: 1rem;
-         font-weight: 700;
+      h3{
+         font-size: 2.5rem;
+         font-weight: bold;
       }
-      p {
-         font-size: 1rem;
-         font-weight: 400;
+      p{
+         font-size: 2rem;
+         font-weight: normal;
       }
-   }
+    }
+  }
 
-   .wrapper {
-      display: flex;
-      justify-content: center;
-      /* align-items: center; */
-      padding-top: 15%;
-      height: 100%;
-   }
-   .scene {
-      width: 260px;
-      height: 200px;
-      perspective: 1000px;
-      position: relative;
-   }
-   .carousel {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      overflow: visible;
-      transform: translateZ(-288px);
-      transform-style: preserve-3d;
-   }
-   .carousel__cell {
-      position: absolute;
-      width: 350px;
-      left: 10px;
-      height: 300px;
-      border: 1px solid rgba(0, 0, 0, 0.3);
-   }
+  .navigation-wrapper {
+    position: relative;
+  }
+
+  .dots {
+    display: flex;
+    padding: 10px 0;
+    justify-content: center;
+  }
+
+  .dot {
+    border: none;
+    width: 10px;
+    height: 10px;
+    background: #c5c5c5;
+    border-radius: 50%;
+    margin: 0 5px;
+    padding: 5px;
+    cursor: pointer;
+  }
+
+  .dot:focus {
+    outline: none;
+  }
+
+  .dot.active {
+    background: #000;
+  }
+
+  .arrow {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    -webkit-transform: translateY(-50%);
+    fill: #000;
+    cursor: pointer;
+  }
+
+  .arrow--left {
+    left: 5px;
+  }
+
+  .arrow--right {
+    left: auto;
+    right: 5px;
+  }
+
+  .arrow--disabled {
+    fill: rgba(0, 0, 0, 0.5);
+  }
 `;
